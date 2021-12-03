b0f6e10086d9f6846c49bedfc611c1601f78b4ff
Sandeep Chinchali <sandeepc@utexas.edu>
2021-12-02 21:17:58 -0800

    updates

--- src/basic_neural_network_classifier.py
+++ src/basic_neural_network_classifier.py
@@ -7,6 +7,7 @@ import pandas
 import sys,os
 from collections import OrderedDict
 import argparse
+import numpy as np
 
 # generic plotting utils - always use and modify these
 # UTILS_DIR=os.environ['UTILS_DIR']
@@ -73,6 +74,8 @@ if __name__ == '__main__':
 	# 3.0    526
 	# Name: Activity, dtype: int64
 
+    num_activities = len(set(train_df['Activity']))
+
     # get a numpy array of X and Y data
     ########################################################################
 
@@ -115,11 +118,13 @@ if __name__ == '__main__':
     num_sensors = int(len(x_features_columns)/num_features)
 
     # first, convert the SCALED training data to a pytorch tensor
-    torch_train_x_tensor = torch.tensor(train_x_np_scaled)
-    torch_train_y_tensor = torch.tensor(train_y_np)
+    torch_train_x_tensor = torch.tensor(train_x_np_scaled.astype(np.float32))
+    #torch_train_y_tensor = torch.tensor(train_y_np.astype(np.float32))
+    torch_train_y_tensor = torch.tensor(train_y_np).type(torch.LongTensor)
 
-    torch_val_x_tensor = torch.tensor(val_x_np_scaled)
-    torch_val_y_tensor = torch.tensor(val_y_np)
+    torch_val_x_tensor = torch.tensor(val_x_np_scaled.astype(np.float32))
+    #torch_val_y_tensor = torch.tensor(val_y_np.astype(np.float32))
+    torch_val_y_tensor = torch.tensor(val_y_np).type(torch.LongTensor)
 
     print(' ')
     print('training tensors: ')
@@ -161,11 +166,123 @@ if __name__ == '__main__':
     # BATCH_SIZE x num_sensors x num_features, where BATCH_SIZE is a default of 32
     ########################################################################
 
-    train_dataset, train_dataloader = get_pytorch_dataloader(train_x_tensor_resized, torch_train_y_tensor, params=DEFAULT_PARAMS, print_mode = True, max_print=5)
+    train_dataset, train_loader = get_pytorch_dataloader(train_x_tensor_resized, torch_train_y_tensor, params=DEFAULT_PARAMS, print_mode = False, max_print=5)
 
-    val_dataset, val_dataloader = get_pytorch_dataloader(val_x_tensor_resized, torch_val_y_tensor, params=DEFAULT_PARAMS, print_mode = True, max_print=5)
+    val_dataset, val_loader = get_pytorch_dataloader(val_x_tensor_resized, torch_val_y_tensor, params=DEFAULT_PARAMS, print_mode = False, max_print=5)
 
 
     # STEP 3: CREATE A SIMPLE NEURAL NETWORK MODEL, LOSS FUNCTION, OPTIMIZER
     ########################################################################
 
+    train_options = {"train_loader": train_loader,
+                     "val_loader": val_loader,
+                     "num_epochs": 10,
+                     "batch_size": 32,
+                     "learning_rate": 1e-3,
+                     "output_freq": 1,
+                     "checkpoint_freq": 5,
+                     "save_model": False,
+                     "model_save_path": None,
+                     "plot_dir": None}
+
+
+    # set up model (defined in utils)
+    model = BasicConvNet(num_classes = num_activities)
+
+    # set up an optimizer
+    optimizer = torch.optim.Adam(model.parameters(),
+                              lr=train_options["learning_rate"], amsgrad=True)
+
+    # loss/error function
+    loss_func = torch.nn.CrossEntropyLoss()
+
+    # save the loss in arrays
+    train_loss_vec = []
+    val_loss_vec = []
+
+    # loop through the data for NUM_EPOCHS iterations to learn the model
+    for i in range(train_options["num_epochs"]):
+        train_loss = 0
+        for batch_num, (batch_x, batch_y) in enumerate(train_options['train_loader']):
+
+            #batch_x_reshaped, batch_y_reshaped = resize_batch_per_model_type(batch_x, batch_y)
+            batch_x_reshaped = batch_x.unsqueeze(1)
+            batch_y_reshaped = batch_y
+
+            print(' ')
+            print(batch_x.shape, batch_x_reshaped.shape)
+            print(batch_y.shape, batch_y_reshaped.shape)
+            print(' ')
+
+            # forward pass
+            predictions = model(batch_x_reshaped)
+
+            # backwards pass (backpropagate gradients)
+            loss = loss_func(predictions, batch_y_reshaped)
+
+            # add to running sum of loss
+            train_loss += loss.item()
+
+            # take a gradient descent step
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # how often to print loss
+        if (i + 1) % train_options["output_freq"] == 0:
+            print(" ")
+            train_loss_vec.append(train_loss)
+            print("Epoch %d: Train loss %0.3f" % (i + 1, train_loss))
+
+            # now freeze the model periodically to test it on validation data
+            model.eval()
+            with torch.no_grad():
+                val_loss = 0
+                for batch_num, (val_x, val_y) in enumerate(train_options['val_loader']):
+
+                    val_x_reshaped = val_x.unsqueeze(1)
+                    val_y_reshaped = val_y
+
+                    predictions = model(val_x_reshaped)
+                    val_loss += loss_func(predictions, val_y_reshaped)
+
+                print("Epoch %d: Val loss %0.3f" % (i + 1, val_loss))
+                val_loss_vec.append(val_loss.item())
+
+            print(" ")
+            # reset the model for training
+            model.train()
+
+    print(' ')
+    print('train_loss')
+    print(train_loss_vec)
+    print(' ')
+
+    print(' ')
+    print('val_loss')
+    print(val_loss_vec)
+    print(' ')
+
+    ## plot the losses and optionally save the model to a file on disk
+    #model_name = "_".join((model.name,
+    #                       "Epochs"+str(train_options["num_epochs"]),
+    #                       "LatentDim"+str(model_params["latent_dim"])))
+
+    #plt.plot(np.array(train_loss[1:]))
+    #plt.plot(np.array(val_loss[1:]))
+    #plt.legend(["train", "val"])
+    #plt.xlabel("Epoch")
+    #plt.ylabel("loss")
+    #plt.title("Prediction Model")
+    #plt.savefig(train_options['plot_dir'] + '/' + model_name)
+
+    #if train_options["save_model"]:
+    #    model_path = train_options["model_save_path"] + model_name + '.pt'
+    #    print('model_path: ', model_path)
+
+    #    model_save_dict = {'epoch': i, 'model_state_dict': model.state_dict(), \
+    #        'optimizer_state_dict': optimizer.state_dict()}
+
+    #    torch.save(model_save_dict, model_path)
+
+    #return model
