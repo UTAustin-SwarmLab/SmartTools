d76864ba5fe71b58c0634fdd94456a0be647c573
Sandeep Chinchali <sandeepc@utexas.edu>
2021-12-03 15:29:33 -0800

    updates

--- src/test_pretrained_neural_network.py
+++ src/test_pretrained_neural_network.py
@@ -52,6 +52,8 @@ if __name__ == '__main__':
 
     parser.add_argument('--val_csv', type=str, default='../processed_data/Feature_Processed_Data/OL50_10secframe_Proccessed_Validate_Xy_Matrix.csv')
 
+    parser.add_argument('--test_csv', type=str, default='../processed_data/Feature_Processed_Data/OL50_10secframe_Proccessed_Test_Xy_Matrix.csv')
+
     args = parser.parse_args()
 
     # STEP 0: PANDAS DATA MANIPULATION, SAME AS RANDOM FOREST
@@ -59,24 +61,7 @@ if __name__ == '__main__':
     # Load into dataframes and do some basic stats
     train_df = pandas.read_csv(args.train_csv)
     val_df = pandas.read_csv(args.val_csv)
-    # write the shapes of the dataframes
-    print(train_df.shape)
-
-	# (2160, 114)
-
-    print(val_df.shape)
-	# (720, 114)
-
-    print(train_df.describe())
-    # will print stats of each column, clearly see there are outliers
-
-    print(train_df['Activity'].value_counts())
-	# >>> train_df['Activity'].value_counts()
-	# 0.0    553
-	# 1.0    546
-	# 2.0    535
-	# 3.0    526
-	# Name: Activity, dtype: int64
+    test_df = pandas.read_csv(args.test_csv)
 
     num_activities = len(set(train_df['Activity']))
 
@@ -95,85 +80,56 @@ if __name__ == '__main__':
     y_features_columns = 'Activity'
 
     # repeat key column extraction for train and val data
-    train_x_np, train_y_np, train_x_df, train_y_df = get_xy_numpy(train_df, x_features_columns, y_features_columns=y_features_columns)
 
-    val_x_np, val_y_np, val_x_df, val_y_df = get_xy_numpy(val_df, x_features_columns, y_features_columns=y_features_columns)
+    train_test_val_df_dict = OrderedDict()
+    train_test_val_df_dict['train'] = train_df
+    train_test_val_df_dict['val'] = val_df
+    train_test_val_df_dict['test'] = test_df
 
-    scaler = MinMaxScaler()
-
-    # fit the params of scaling on TRAIN ONLY
-    FittedScaler = scaler.fit(train_x_np)
-
-    # now actually transform the training data
-    train_x_np_scaled = FittedScaler.transform(train_x_np)
-
-    # use the SAME transformation on the test data
-    val_x_np_scaled = FittedScaler.transform(val_x_np)
-
-    # STEP 1: CREATE PYTORCH TENSORS
-    ########################################################################
-
-    # now, create pytorch tensors for the numpy scaled data before feeding it into a DNN
-    ########################################################################
+    train_test_val_dataloaders = OrderedDict()
 
     # min, max, mean etc.
     num_features = 10
 
-    num_sensors = int(len(x_features_columns)/num_features)
+    for data_split, data_df in train_test_val_df_dict.iteritems():
 
-    # first, convert the SCALED training data to a pytorch tensor
-    torch_train_x_tensor = torch.tensor(train_x_np_scaled.astype(np.float32))
-    #torch_train_y_tensor = torch.tensor(train_y_np.astype(np.float32))
-    torch_train_y_tensor = torch.tensor(train_y_np).type(torch.LongTensor)
+        data_x_np, data_y_np, data_x_df, data_y_df = get_xy_numpy(train_df, x_features_columns, y_features_columns=y_features_columns)
 
-    torch_val_x_tensor = torch.tensor(val_x_np_scaled.astype(np.float32))
-    #torch_val_y_tensor = torch.tensor(val_y_np.astype(np.float32))
-    torch_val_y_tensor = torch.tensor(val_y_np).type(torch.LongTensor)
+        if data_split == 'Train':
+            scaler = MinMaxScaler()
 
-    print(' ')
-    print('training tensors: ')
-    print(torch_train_x_tensor.shape, torch_train_y_tensor.shape)
-    print(' ')
+            # fit the params of scaling on TRAIN ONLY
+            FittedScaler = scaler.fit(data_x_np)
 
-    print(' ')
-    print('val tensors: ')
-    print(torch_val_x_tensor.shape, torch_val_y_tensor.shape)
-    print(' ')
+        # now actually transform the training data
+        data_x_np_scaled = FittedScaler.transform(data_x_np)
 
-	# current outputs
-	# training tensors:
-	# torch.Size([2160, 110]) torch.Size([2160])
+        num_sensors = int(len(x_features_columns)/num_features)
 
-	# val tensors:
-	# torch.Size([720, 110]) torch.Size([720])
+        # first, convert the SCALED training data to a pytorch tensor
+        torch_x_tensor = torch.tensor(data_x_np_scaled.astype(np.float32))
+        torch_y_tensor = torch.tensor(data_y_np).type(torch.LongTensor)
 
-    # KEY next step: now, resize the data so each example is a num_sensor x num_feature matrix
-    # so it can nicely be fed into a CNN. the CNN will aggregate features across sensors
-    # in our case since we have 11 sensors and 10 features, we should get tensors of size
-    # (NUM_EXAMPLES x 11 x 10)
-    #####################################################################
+        # KEY next step: now, resize the data so each example is a num_sensor x num_feature matrix
+        # so it can nicely be fed into a CNN. the CNN will aggregate features across sensors
+        # in our case since we have 11 sensors and 10 features, we should get tensors of size
+        # (NUM_EXAMPLES x 11 x 10)
+        #####################################################################
 
-    train_x_tensor_resized = torch_train_x_tensor.reshape([-1, num_sensors, num_features])
-    val_x_tensor_resized = torch_val_x_tensor.reshape([-1, num_sensors, num_features])
+        data_x_tensor_resized = torch_train_x_tensor.reshape([-1, num_sensors, num_features])
 
-    print(train_x_tensor_resized.shape, val_x_tensor_resized.shape)
-    # sanity check: indeed, they have the correct size!
-    # torch.Size([2160, 11, 10]) torch.Size([720, 11, 10])
+        # STEP 2: CREATE PYTORCH DATALOADERS TO CYCLE THROUGH DATA
+        # from now on, all helper functions are in neural_network_utils.py
+        ########################################################################
 
+        # now that we have correctly shaped tensors, lets create a pytorch dataloader
+        # to cycle through data. this will print data in chunks of:
+        # BATCH_SIZE x num_sensors x num_features, where BATCH_SIZE is a default of 32
+        ########################################################################
 
-    # STEP 2: CREATE PYTORCH DATALOADERS TO CYCLE THROUGH DATA
-    # from now on, all helper functions are in neural_network_utils.py
-    ########################################################################
+        data_dataset, data_loader = get_pytorch_dataloader(data_x_tensor_resized, torch_y_tensor, params=DEFAULT_PARAMS, print_mode = False, max_print=5)
 
-    # now that we have correctly shaped tensors, lets create a pytorch dataloader
-    # to cycle through data. this will print data in chunks of:
-    # BATCH_SIZE x num_sensors x num_features, where BATCH_SIZE is a default of 32
-    ########################################################################
-
-    train_dataset, train_loader = get_pytorch_dataloader(train_x_tensor_resized, torch_train_y_tensor, params=DEFAULT_PARAMS, print_mode = False, max_print=5)
-
-    val_dataset, val_loader = get_pytorch_dataloader(val_x_tensor_resized, torch_val_y_tensor, params=DEFAULT_PARAMS, print_mode = False, max_print=5)
-
+        train_test_val_dataloaders[data_split] = data_loader
 
     # STEP 3: TEST A PRE-TRAINED NEURAL NETWORK MODEL
     ########################################################################
@@ -187,14 +143,10 @@ if __name__ == '__main__':
     MODEL_DIR = SCRATCH_DIR + '/models/'
     remove_and_create_dir(MODEL_DIR)
 
-    train_options = {"train_loader": train_loader,
+    test_options = {"train_loader": train_loader,
                      "val_loader": val_loader,
-                     "num_epochs": 50,
+                     "test_loader": test_loader,
                      "batch_size": 64,
-                     "learning_rate": 1e-3,
-                     "output_freq": 1,
-                     "checkpoint_freq": 5,
-                     "save_model": True,
                      "model_save_path": MODEL_DIR,
                      "plot_dir": PLOT_DIR,
                      "model_name": 'ConvNet Tool Activity Classifier'}
@@ -207,10 +159,8 @@ if __name__ == '__main__':
     loss_func = torch.nn.CrossEntropyLoss()
 
     # load in the pretrained weights
-    model_save_dict = {'epoch': i, 'model_state_dict': model.state_dict(), \
-        'optimizer_state_dict': optimizer.state_dict()}
-
-    torch.save(model_save_dict, model_path)
+    MODEL_PATH = MODEL_DIR + '/' + test_options['model_name'] + '.pt'
+    model.load_state_dict(torch.load(MODEL_PATH))
 
     # prepare to count predictions for each class
     correct_pred = {classname: 0 for classname in classes}
