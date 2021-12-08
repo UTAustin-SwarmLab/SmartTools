beb0743d4b439d20806efde7ff5deddd96817fc5
Sandeep Chinchali <sandeepc@utexas.edu>
2021-12-07 22:55:50 -0800

    updates

---
+++ src/tensorflow_lite_DNN/plot_loss.py
@@ -0,0 +1,39 @@
+import pandas
+import numpy as np
+import sys,os
+
+# where the base code is on your machine
+SMART_TOOLS_ROOT_DIR = os.environ['SMART_TOOLS_ROOT_DIR']
+SCRATCH_DIR = SMART_TOOLS_ROOT_DIR + '/scratch/'
+
+# generic plotting utils - always use and modify these
+UTILS_DIR=os.environ['UTILS_DIR']
+sys.path.append(UTILS_DIR)
+from plotting_utils import *
+from textfile_utils import *
+
+
+if __name__ == '__main__':
+
+    df = pandas.read_csv('training.log')
+    train_accuracy_vec = np.array(df['accuracy'])
+    val_accuracy_vec = np.array(df['val_accuracy'])
+
+    print(' ')
+    print('train_loss')
+    print(train_accuracy_vec)
+    print(' ')
+
+    print(' ')
+    print('val_loss')
+    print(val_accuracy_vec)
+    print(' ')
+
+    plt.plot(np.array(train_accuracy_vec))
+    plt.plot(np.array(val_accuracy_vec))
+    plt.legend(["train", "val"])
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.title('TF Model')
+    plt.savefig('accuracy.pdf')
+    plt.close()
