e09495e135d954349a29e607de8d60255f759adb
Sandeep Chinchali <sandeepc@utexas.edu>
2021-12-08 20:43:54 -0800

    start to clean up all code

--- src/tensorflow_lite_DNN/tf_train_activity_classifier.py
+++ src/tensorflow_lite_DNN/tf_train_activity_classifier.py
@@ -1,185 +1,199 @@
-"""Build and train neural networks."""
+'''
+    Use a neural network in pytorch to classify different activity styles
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+'''
 
+import pandas
+import sys,os
+from collections import OrderedDict
 import argparse
-import datetime
-import os
-from data_load import DataLoader
-
 import numpy as np
+import datetime
+
+# where the base code is on your machine
+SMART_TOOLS_ROOT_DIR = os.environ['SMART_TOOLS_ROOT_DIR']
+SCRATCH_DIR = SMART_TOOLS_ROOT_DIR + '/scratch/'
+
+# generic plotting utils - always use and modify these
+UTILS_DIR=os.environ['UTILS_DIR']
+sys.path.append(UTILS_DIR)
+from plotting_utils import *
+from textfile_utils import *
+
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import tensorflow as tf
+
+from utils_tensorflow import *
+
+# where the tensorflow logs are placed
 
 logdir = "logs/scalars/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
+# helper function to extract key columns from the pandas dataframes
 
-def reshape_function(data, label):
-  reshaped_data = tf.reshape(data, [-1, 3, 1])
-  return reshaped_data, label
+def get_xy_numpy(df, x_features_columns, y_features_columns='Activity'):
+    x_df = df[x_features_columns]
+    x_np = x_df.to_numpy()
+    # get the output column we want to predict
+    y_df = df[y_features_columns]
+    y_np = y_df.to_numpy()
+    # assert the x and y dataframes do NOT have any null or NaN entries
+    assert(x_df.isnull().sum().sum() == 0)
+    assert(y_df.isnull().sum().sum() == 0)
 
-
-def calculate_model_size(model):
-  print(model.summary())
-  var_sizes = [
-      np.product(list(map(int, v.shape))) * v.dtype.size
-      for v in model.trainable_variables
-  ]
-  print("Model size:", sum(var_sizes) / 1024, "KB")
-
-
-def build_cnn(seq_length):
-  """Builds a convolutional neural network in Keras."""
-  model = tf.keras.Sequential([
-      tf.keras.layers.Conv2D(
-          8, (4, 3),
-          padding="same",
-          activation="relu",
-          input_shape=(seq_length, 3, 1)),  # output_shape=(batch, 128, 3, 8)
-      tf.keras.layers.MaxPool2D((3, 3)),  # (batch, 42, 1, 8)
-      tf.keras.layers.Dropout(0.1),  # (batch, 42, 1, 8)
-      tf.keras.layers.Conv2D(16, (4, 1), padding="same",
-                             activation="relu"),  # (batch, 42, 1, 16)
-      tf.keras.layers.MaxPool2D((3, 1), padding="same"),  # (batch, 14, 1, 16)
-      tf.keras.layers.Dropout(0.1),  # (batch, 14, 1, 16)
-      tf.keras.layers.Flatten(),  # (batch, 224)
-      tf.keras.layers.Dense(16, activation="relu"),  # (batch, 16)
-      tf.keras.layers.Dropout(0.1),  # (batch, 16)
-      tf.keras.layers.Dense(4, activation="softmax")  # (batch, 4)
-  ])
-  model_path = os.path.join("./netmodels", "CNN")
-  print("Built CNN.")
-  if not os.path.exists(model_path):
-    os.makedirs(model_path)
-  model.load_weights("./netmodels/CNN/weights.h5")
-  return model, model_path
+    return x_np, y_np, x_df, y_df
 
 
-def build_lstm(seq_length):
-  """Builds an LSTM in Keras."""
-  model = tf.keras.Sequential([
-      tf.keras.layers.Bidirectional(
-          tf.keras.layers.LSTM(22),
-          input_shape=(seq_length, 3)),  # output_shape=(batch, 44)
-      tf.keras.layers.Dense(4, activation="sigmoid")  # (batch, 4)
-  ])
-  model_path = os.path.join("./netmodels", "LSTM")
-  print("Built LSTM.")
-  if not os.path.exists(model_path):
-    os.makedirs(model_path)
-  return model, model_path
+# how many samples are taken at once for training
+BATCH_SIZE = 64
+SHUFFLE_BUFFER_SIZE = 100
+
+# NOTE: data will be re-organized to size:
+# BATCH_SIZE x NUM_SENSORS x NUM_FEATURES
+# view this as an image with 1 channel, NUM_SENSORS x NUM_FEATURES size
+
+if __name__ == '__main__':
+
+    # Pull arguments from command line.
+    parser = argparse.ArgumentParser(description='plot sensor data')
+
+    # where to read data
+    parser.add_argument('--train_csv', type=str, default= SMART_TOOLS_ROOT_DIR + '/processed_data/Feature_Processed_Data/OL50_10secframe_Proccessed_Train_Xy_Matrix.csv')
+
+    parser.add_argument('--val_csv', type=str, default= SMART_TOOLS_ROOT_DIR + '/processed_data/Feature_Processed_Data/OL50_10secframe_Proccessed_Validate_Xy_Matrix.csv')
+
+    parser.add_argument('--test_csv', type=str, default=SMART_TOOLS_ROOT_DIR + '/processed_data/Feature_Processed_Data/OL50_10secframe_Proccessed_Test_Xy_Matrix.csv')
+
+    args = parser.parse_args()
+
+    # STEP 0: PANDAS DATA MANIPULATION, SAME AS RANDOM FOREST
+    ########################################################################
+    # Load into dataframes and do some basic stats
+    train_df = pandas.read_csv(args.train_csv)
+    val_df = pandas.read_csv(args.val_csv)
+    test_df = pandas.read_csv(args.test_csv)
+
+    num_activities = len(set(train_df['Activity']))
+
+    # get a numpy array of X and Y data
+    ########################################################################
+
+    # get all columns that are inputs to our model
+    x_features_columns = [colname for colname in list(train_df) if colname not in ['Unnamed: 0', 'Activity', 'Subject Number', 'Trial']]
+
+    # try different y columns
+    # first, how well can we predict the activity?
+    # then, can we predict the subject
+    # then, we can predict the trial number
+    # the first should be high, second and third should be low
+
+    y_features_columns = 'Activity'
+
+    # repeat key column extraction for train and val data
+
+    train_test_val_df_dict = OrderedDict()
+    train_test_val_df_dict['train'] = train_df
+    train_test_val_df_dict['val'] = val_df
+    train_test_val_df_dict['test'] = test_df
+
+    tf_dataset_dict = OrderedDict()
+
+    # min, max, mean etc.
+    num_features = 10
+
+    num_sensors = int(len(x_features_columns)/num_features)
+
+    # SCALE THE DATA and create a Tensorflow DataSet
+    # analagous to a Pytorch data loader
+
+    for data_split, data_df in train_test_val_df_dict.items():
+
+        data_x_np, data_y_np, data_x_df, data_y_df = get_xy_numpy(data_df, x_features_columns, y_features_columns=y_features_columns)
+
+        if data_split == 'train':
+            scaler = MinMaxScaler()
+
+            # fit the params of scaling on TRAIN ONLY
+            FittedScaler = scaler.fit(data_x_np)
+
+        # now actually transform the training data
+        data_x_np_scaled = FittedScaler.transform(data_x_np)
+
+        # BATCH_SIZE x NUM_SENSORS x NUM_FEATURES
+        # view this as an image with 1 channel, NUM_SENSORS x NUM_FEATURES size
+
+        reshaped_data_x_np_scaled = data_x_np_scaled.reshape([-1, num_sensors, num_features])
+
+        # x: data_x_np_scaled
+        # y: data_y_np
+        print(' ')
+        print(' ')
+        print('data_split: ', data_split)
+        print('data_x_np: ', data_x_np_scaled.shape)
+        print('reshaped_data_x_np: ', reshaped_data_x_np_scaled.shape)
+        print('data_y_np: ', data_y_np.shape)
+        print(' ')
+        print(' ')
+
+        # get a tensorflow dataset
+        tf_dataset = tf.data.Dataset.from_tensor_slices((reshaped_data_x_np_scaled, data_y_np))
+
+        # load the tensorflow dataset
+        tf_dataset_dict[data_split] = tf_dataset
 
 
-def load_data(train_data_path, valid_data_path, test_data_path, seq_length):
-  data_loader = DataLoader(train_data_path,
-                           valid_data_path,
-                           test_data_path,
-                           seq_length=seq_length)
-  data_loader.format()
-  return data_loader.train_len, data_loader.train_data, data_loader.valid_len, \
-      data_loader.valid_data, data_loader.test_len, data_loader.test_data
+    # we now have all the datasets and dataloaders in TENSORFLOW format
+    train_data = tf_dataset_dict['train'].shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+    val_data = tf_dataset_dict['val'].batch(BATCH_SIZE)
+    test_data = tf_dataset_dict['test'].batch(BATCH_SIZE)
 
+    # find the number of batches in the TESTING dataset
+    # this is useful later
+    test_len = 0
+    for batch in test_data:
+        test_len += 1
 
-def build_net(args, seq_length):
-  if args.model == "CNN":
-    model, model_path = build_cnn(seq_length)
-  elif args.model == "LSTM":
-    model, model_path = build_lstm(seq_length)
-  else:
-    print("Please input correct model name.(CNN  LSTM)")
-  return model, model_path
+    # now build the CNN model
+    base_dir = SCRATCH_DIR + '/tensorflow_classifier/'
+    remove_and_create_dir(base_dir)
 
+    model_base_dir = base_dir + '/tf_model/'
+    remove_and_create_dir(model_base_dir)
 
-def train_net(
-    model,
-    model_path,  # pylint: disable=unused-argument
-    train_len,  # pylint: disable=unused-argument
-    train_data,
-    valid_len,
-    valid_data,  # pylint: disable=unused-argument
-    test_len,
-    test_data,
-    kind):
-  """Trains the model."""
-  calculate_model_size(model)
-  epochs = 50
-  batch_size = 64
-  model.compile(optimizer="adam",
+    # 1D CNN model
+    model, model_path = build_1D_CNN(model_base_dir, model_name = '1DCNN', num_sensors = num_sensors, num_features = num_features, num_outputs = num_activities)
+
+    # How many KB is the final tensorflow model?
+    model_size = calculate_model_size(model)
+
+    # when we train, we save a csv of the training accuracy/loss
+    csv_logger = tf.keras.callbacks.CSVLogger(base_dir + '/training.log')
+
+    # how long we train, set up model with loss function
+    epochs = 50
+    batch_size = 64
+    model.compile(optimizer="adam",
                 loss="sparse_categorical_crossentropy",
                 metrics=["accuracy"])
-  if kind == "CNN":
-    train_data = train_data.map(reshape_function)
-    test_data = test_data.map(reshape_function)
-    valid_data = valid_data.map(reshape_function)
-  test_labels = np.zeros(test_len)
-  idx = 0
-  for data, label in test_data:  # pylint: disable=unused-variable
-    test_labels[idx] = label.numpy()
-    idx += 1
-  train_data = train_data.batch(batch_size).repeat()
-  valid_data = valid_data.batch(batch_size)
-  test_data = test_data.batch(batch_size)
-  model.fit(train_data,
+
+    #test_labels = np.zeros(test_len)
+    #idx = 0
+    #for data, label in test_data:  # pylint: disable=unused-variable
+    #    test_labels[idx] = label.numpy()
+    #    idx += 1
+
+    # now, finally start training
+    model.fit(train_data,
             epochs=epochs,
-            validation_data=valid_data,
-            steps_per_epoch=1000,
-            validation_steps=int((valid_len - 1) / batch_size + 1),
-            callbacks=[tensorboard_callback])
-  loss, acc = model.evaluate(test_data)
-  pred = np.argmax(model.predict(test_data), axis=1)
-  confusion = tf.math.confusion_matrix(labels=tf.constant(test_labels),
-                                       predictions=tf.constant(pred),
-                                       num_classes=4)
-  print(confusion)
-  print("Loss {}, Accuracy {}".format(loss, acc))
-  # Convert the model to the TensorFlow Lite format without quantization
-  converter = tf.lite.TFLiteConverter.from_keras_model(model)
-  tflite_model = converter.convert()
+            validation_data=val_data,
+            callbacks=[tensorboard_callback, csv_logger])
 
-  # Save the model to disk
-  open("model.tflite", "wb").write(tflite_model)
+    loss, acc = model.evaluate(test_data)
+    pred = np.argmax(model.predict(test_data), axis=1)
 
-  # Convert the model to the TensorFlow Lite format with quantization
-  converter = tf.lite.TFLiteConverter.from_keras_model(model)
-  converter.optimizations = [tf.lite.Optimize.DEFAULT]
-  tflite_model = converter.convert()
-
-  # Save the model to disk
-  open("model_quantized.tflite", "wb").write(tflite_model)
-
-  basic_model_size = os.path.getsize("model.tflite")
-  print("Basic model is %d bytes" % basic_model_size)
-  quantized_model_size = os.path.getsize("model_quantized.tflite")
-  print("Quantized model is %d bytes" % quantized_model_size)
-  difference = basic_model_size - quantized_model_size
-  print("Difference is %d bytes" % difference)
-
-
-if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--model", "-m")
-  parser.add_argument("--person", "-p")
-  args = parser.parse_args()
-
-  seq_length = 128
-
-  print("Start to load data...")
-  if args.person == "true":
-    train_len, train_data, valid_len, valid_data, test_len, test_data = \
-        load_data("./person_split/train", "./person_split/valid",
-                  "./person_split/test", seq_length)
-  else:
-    train_len, train_data, valid_len, valid_data, test_len, test_data = \
-        load_data("./data/train", "./data/valid", "./data/test", seq_length)
-
-  print("Start to build net...")
-  model, model_path = build_net(args, seq_length)
-
-  print("Start training...")
-  train_net(model, model_path, train_len, train_data, valid_len, valid_data,
-            test_len, test_data, args.model)
-
-  print("Training finished!")
-
+    #confusion = tf.math.confusion_matrix(labels=tf.constant(test_labels),
+    #                                   predictions=tf.constant(pred),
+    #                                   num_classes=4)
+    #print(confusion)
+    print("Test Loss {}, Test Accuracy {}".format(loss, acc))
--- src/tensorflow_lite_DNN/utils_tensorflow.py
+++ src/tensorflow_lite_DNN/utils_tensorflow.py
@@ -1,6 +1,102 @@
 import tensorflow as tf
 
-def reshape_function(data, label, num_sensors=11, num_features=10, num_channels=1):
-  reshaped_data = tf.reshape(data, [-1, num_sensors, num_features])
-  return reshaped_data, label
 
+def calculate_model_size(model):
+  print(model.summary())
+  var_sizes = [
+      np.product(list(map(int, v.shape))) * v.dtype.size
+      for v in model.trainable_variables
+  ]
+  model_size = sum(var_sizes) / 1024
+
+  print("Model size:", model_size, "KB")
+  return model_size
+
+
+def build_1D_CNN(model_base_dir, model_name = '1DCNN', num_sensors = 11, num_features = 10, num_outputs = 4):
+
+  """Builds a convolutional neural network in Keras."""
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv1D(56, 3, activation='relu', input_shape=(num_sensors, num_features)),
+        tf.keras.layers.MaxPooling1D(2, 2),
+        tf.keras.layers.Conv1D(56, 3, activation='relu'),
+        tf.keras.layers.MaxPooling1D(2, 2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(784, activation='relu'),
+        tf.keras.layers.Dense(num_outputs, activation='softmax')
+    ])
+
+  model_path = None
+
+  #model_path = os.path.join(model_base_dir + "./netmodels", "1DCNN")
+  #print("Built CNN.")
+  #if not os.path.exists(model_path):
+  #  os.makedirs(model_path)
+  #model.load_weights(model_base_dir + "./netmodels/1DCNN/weights.h5")
+
+  return model, model_path
+
+
+def train_net(
+    model,
+    model_path,  # pylint: disable=unused-argument
+    train_len,  # pylint: disable=unused-argument
+    train_data,
+    valid_len,
+    valid_data,  # pylint: disable=unused-argument
+    test_len,
+    test_data,
+    kind):
+  """Trains the model."""
+  calculate_model_size(model)
+  epochs = 50
+  batch_size = 64
+  model.compile(optimizer="adam",
+                loss="sparse_categorical_crossentropy",
+                metrics=["accuracy"])
+  if kind == "CNN":
+    train_data = train_data.map(reshape_function)
+    test_data = test_data.map(reshape_function)
+    valid_data = valid_data.map(reshape_function)
+  test_labels = np.zeros(test_len)
+  idx = 0
+  for data, label in test_data:  # pylint: disable=unused-variable
+    test_labels[idx] = label.numpy()
+    idx += 1
+  train_data = train_data.batch(batch_size).repeat()
+  valid_data = valid_data.batch(batch_size)
+  test_data = test_data.batch(batch_size)
+  model.fit(train_data,
+            epochs=epochs,
+            validation_data=valid_data,
+            steps_per_epoch=1000,
+            validation_steps=int((valid_len - 1) / batch_size + 1),
+            callbacks=[tensorboard_callback])
+  loss, acc = model.evaluate(test_data)
+  pred = np.argmax(model.predict(test_data), axis=1)
+  confusion = tf.math.confusion_matrix(labels=tf.constant(test_labels),
+                                       predictions=tf.constant(pred),
+                                       num_classes=4)
+  print(confusion)
+  print("Loss {}, Accuracy {}".format(loss, acc))
+  # Convert the model to the TensorFlow Lite format without quantization
+  converter = tf.lite.TFLiteConverter.from_keras_model(model)
+  tflite_model = converter.convert()
+
+  # Save the model to disk
+  open("model.tflite", "wb").write(tflite_model)
+
+  # Convert the model to the TensorFlow Lite format with quantization
+  converter = tf.lite.TFLiteConverter.from_keras_model(model)
+  converter.optimizations = [tf.lite.Optimize.DEFAULT]
+  tflite_model = converter.convert()
+
+  # Save the model to disk
+  open("model_quantized.tflite", "wb").write(tflite_model)
+
+  basic_model_size = os.path.getsize("model.tflite")
+  print("Basic model is %d bytes" % basic_model_size)
+  quantized_model_size = os.path.getsize("model_quantized.tflite")
+  print("Quantized model is %d bytes" % quantized_model_size)
+  difference = basic_model_size - quantized_model_size
+  print("Difference is %d bytes" % difference)
