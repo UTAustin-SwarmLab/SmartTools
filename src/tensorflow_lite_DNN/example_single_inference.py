9994c7687b3a7dc7b86ecfa3684c1cd0238bb02b
Sandeep Chinchali <sandeepc@utexas.edu>
2021-12-10 20:34:04 -0800

    updates

--- src/tensorflow_lite_DNN/example_single_inference.py
+++ src/tensorflow_lite_DNN/example_single_inference.py
@@ -6,11 +6,32 @@ import sys,os
 import numpy as np
 import tensorflow as tf
 
+
+# where the base code is on your machine
+SMART_TOOLS_ROOT_DIR = os.environ['SMART_TOOLS_ROOT_DIR']
+
 # data is expected as:
 # BATCH_SIZE x NUM_SENSORS x NUM_FEATURES
 # view this as an image with 1 channel, NUM_SENSORS x NUM_FEATURES size
 
 # for a single inference, we have a batch size of 1
+
+
+def load_trained_tflite_model(model_path):
+
+    # Load the TFLite model and allocate tensors.
+    interpreter = tf.lite.Interpreter(model_path)
+    interpreter.allocate_tensors()
+
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    ################################################
+    # Test the model on random input data.
+    input_shape = input_details[0]['shape']
+
+    return interpreter, input_details, output_details, input_shape
 
 if __name__ == '__main__':
 
@@ -24,31 +45,35 @@ if __name__ == '__main__':
     # compare both our quantized and float models
     model_type_list = [('TFLite Quantized', tflite_quantized_model_path), ('TFLite Float', tflite_model_path)]
 
+    NUM_RANDOM_TRIALS = 20
+
     for model_type, model_path in model_type_list:
 
         # actually load the tflite model from disk
         interpreter, input_details, output_details, input_shape = load_trained_tflite_model(model_path)
 
-		for i in NUM_TRIALS:
+        # can just repeat this 1 (without a for loop) for your actual data
 
-			# load in random data of the correct size of 1 x NUM_SENSORS x NUM_FEATURES
-			# we expect this is properly normalized!
-			input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+        for i in range(NUM_RANDOM_TRIALS):
 
-			# actually run the DNN
-			interpreter.set_tensor(input_details[0]['index'], input_data)
-			interpreter.invoke()
+            # load in random data of the correct size of 1 x NUM_SENSORS x NUM_FEATURES
+            # we expect this is properly normalized!
+            input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
 
-			# get the tensor outputs (probability per class)
-			output_data = interpreter.get_tensor(output_details[0]['index'])
+            # actually run the DNN
+            interpreter.set_tensor(input_details[0]['index'], input_data)
+            interpreter.invoke()
 
-			# get the most likely prediction, this is a number between 0 and NUM_CLASSES-1
-			preds = np.squeeze(output_data)
-			argmax_pred = np.argmax(preds)
+            # get the tensor outputs (probability per class)
+            output_data = interpreter.get_tensor(output_details[0]['index'])
 
-			print(' ')
-			print('trial i: ', i)
-			print('preds: ', preds, np.sum(preds))
-			print('argmax_pred: ', argmax_pred)
-			print(' ')
+            # get the most likely prediction, this is a number between 0 and NUM_CLASSES-1
+            preds = np.squeeze(output_data)
+            argmax_pred = np.argmax(preds)
+
+            print(' ')
+            print('trial i: ', i)
+            print('preds: ', preds, np.sum(preds))
+            print('argmax_pred: ', argmax_pred)
+            print(' ')
 
